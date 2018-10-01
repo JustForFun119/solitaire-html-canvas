5c85f10696952aade0de3d75d1f52174363eabf1
Toby Ng <tobyng01@gmail.com>
2018-10-02 01:14:58 +0800

    updated service worker js path

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 (function () {
   // service worker
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
       console.log('service worker: registration successful, scope is:',
         registration);
     }).catch(error => {
